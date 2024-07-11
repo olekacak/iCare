b135c683620dc9c4d8e0ad09102183c5f9e1908c
amier <roslanamier27@gmail.com>
2024-07-11 10:09:02 +0800

    map navigate

--- lib/View/google_map.dart
+++ lib/View/google_map.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:google_maps_webservice/places.dart';
 import 'package:permission_handler/permission_handler.dart';
-import 'package:google_maps_webservice/directions.dart' as directions_pkg show GoogleMapsDirections, TravelMode, Location;
+import 'package:geolocator/geolocator.dart';
+import 'package:google_maps_webservice/places.dart';
+import 'package:google_maps_webservice/directions.dart' as directions_pkg;
 import 'package:flutter_polyline_points/flutter_polyline_points.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class GoogleMapPage extends StatefulWidget {
   @override
@@ -12,17 +14,24 @@ class GoogleMapPage extends StatefulWidget {
 
 class _GoogleMapPageState extends State<GoogleMapPage> {
   GoogleMapController? mapController;
-  final LatLng initialCameraPosition = const LatLng(2.328365, 102.292882);
+  LatLng? _currentLocation;
+  final LatLng initialCameraPosition = const LatLng(2.328365, 102.292882); // Default initial position
   PermissionStatus _permissionStatus = PermissionStatus.denied;
   final TextEditingController _searchController = TextEditingController();
   LatLng? _destination;
+  List<directions_pkg.Route> _routes = [];
   Set<Polyline> _polylines = {};
-  final String apiKey = 'AIzaSyCfVFVYBC6wTOY7fMcoPwtLidl_Wf-KFSk';
+  final String apiKey = 'AIzaSyCfVFVYBC6wTOY7fMcoPwtLidl_Wf-KFSk'; // Replace with your API key
+
+  bool _showNavigationButton = false;
+  String _routeDuration = '';
+  String _routeDistance = '';
 
   @override
   void initState() {
     super.initState();
     _checkPermission();
+    _getCurrentLocation();
   }
 
   Future<void> _checkPermission() async {
@@ -32,29 +41,69 @@ class _GoogleMapPageState extends State<GoogleMapPage> {
     });
   }
 
+  Future<void> _getCurrentLocation() async {
+    try {
+      Position position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+      setState(() {
+        _currentLocation = LatLng(position.latitude, position.longitude);
+      });
+    } catch (e) {
+      print('Error getting current location: $e');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Google Maps Demo'),
-      ),
       body: Stack(
         children: [
           _buildMap(),
           Positioned(
-            top: 16.0,
-            left: 16.0,
-            right: 16.0,
-            child: TextField(
-              controller: _searchController,
-              decoration: InputDecoration(
-                hintText: 'Search destination',
-                suffixIcon: IconButton(
-                  icon: Icon(Icons.search),
-                  onPressed: () => _searchDestination(_searchController.text),
+            top: 1.0,
+            left: 18.0,
+            right: 65.0,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 16.0),
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: Padding(
+                          padding: const EdgeInsets.only(right: 8.0),
+                          child: TextField(
+                            controller: _searchController,
+                            decoration: InputDecoration(
+                              hintText: 'Search destination',
+                              suffixIcon: IconButton(
+                                icon: Icon(Icons.search),
+                                onPressed: () => _searchDestination(_searchController.text),
+                              ),
+                            ),
+                            onSubmitted: _searchDestination,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-              onSubmitted: _searchDestination,
+                if (_permissionStatus == PermissionStatus.denied ||
+                    _permissionStatus == PermissionStatus.permanentlyDenied)
+                  Container(
+                    padding: EdgeInsets.all(8.0),
+                    decoration: BoxDecoration(
+                      color: Colors.redAccent,
+                      borderRadius: BorderRadius.circular(8.0),
+                    ),
+                    child: Text(
+                      _permissionStatus == PermissionStatus.denied
+                          ? 'Permission denied'
+                          : 'Permission permanently denied. Please enable it in settings.',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+              ],
             ),
           ),
         ],
@@ -63,15 +112,23 @@ class _GoogleMapPageState extends State<GoogleMapPage> {
   }
 
   Widget _buildMap() {
-    if (_permissionStatus == PermissionStatus.granted) {
+    if (_permissionStatus == PermissionStatus.granted && _currentLocation != null) {
       return GoogleMap(
         onMapCreated: _onMapCreated,
         initialCameraPosition: CameraPosition(
-          target: initialCameraPosition,
+          target: _currentLocation!,
           zoom: 15.0,
         ),
         myLocationEnabled: true,
         polylines: _polylines,
+        onTap: (LatLng latLng) {
+          setState(() {
+            _destination = latLng;
+            _showNavigationButton = true; // Show navigation button when destination is set
+            _polylines.clear(); // Clear existing polylines
+          });
+          _showRoutes(); // Calculate and display routes for the tapped location
+        },
       );
     } else {
       return Center(
@@ -93,15 +150,9 @@ class _GoogleMapPageState extends State<GoogleMapPage> {
   Future<void> _searchDestination(String query) async {
     print('Searching for destination: $query');
     try {
-      final places = await _getPlaces(query);
-      if (places.isNotEmpty) {
-        final selectedPlace = places.first;
-        final destination = LatLng(selectedPlace.geometry!.location.lat, selectedPlace.geometry!.location.lng);
-        setState(() {
-          _destination = destination;
-        });
-        print('Destination found: $_destination');
-        await _showRoutes();
+      final predictions = await _getPlaces(query);
+      if (predictions.isNotEmpty) {
+        _showPlacesList(predictions);
       } else {
         _showErrorDialog('No results found for your search.');
       }
@@ -111,56 +162,71 @@ class _GoogleMapPageState extends State<GoogleMapPage> {
     }
   }
 
-  Future<void> _showRoutes() async {
-    if (mapController == null || _destination == null) {
+  void _showPlacesList(List<Prediction> predictions) {
+    showModalBottomSheet(
+      context: context,
+      builder: (BuildContext context) {
+        return ListView.builder(
+          itemCount: predictions.length,
+          itemBuilder: (context, index) {
+            final prediction = predictions[index];
+            return ListTile(
+              title: Text(prediction.description ?? 'Unnamed place'),
+              onTap: () {
+                Navigator.pop(context); // Close the search modal sheet
+                _selectPlace(prediction);
+              },
+            );
+          },
+        );
+      },
+    );
+  }
+
+  void _selectPlace(Prediction prediction) async {
+    final details = await _getPlaceDetails(prediction.placeId!);
+    if (details.geometry != null && details.geometry!.location != null) {
+      final destination = LatLng(
+        details.geometry!.location!.lat,
+        details.geometry!.location!.lng,
+      );
+      setState(() {
+        _destination = destination;
+        _showNavigationButton = true; // Show navigation button when destination is set
+      });
+      print('Selected destination: $_destination');
+      _showRoutes();
+    } else {
+      _showErrorDialog('Place details are incomplete.');
+    }
+  }
+
+  void _showRoutes() async {
+    if (mapController == null || _destination == null || _currentLocation == null) {
       return;
     }
 
     try {
-      final currentCameraPosition = await mapController!.getLatLng(ScreenCoordinate(x: 0, y: 0));
-      print('Current camera position: ${currentCameraPosition.latitude}, ${currentCameraPosition.longitude}');
-
       final directions = directions_pkg.GoogleMapsDirections(apiKey: apiKey);
       final response = await directions.directionsWithLocation(
         directions_pkg.Location(
-          lat: currentCameraPosition.latitude,
-          lng: currentCameraPosition.longitude,
+          lat: _currentLocation!.latitude,
+          lng: _currentLocation!.longitude,
         ),
         directions_pkg.Location(
           lat: _destination!.latitude,
           lng: _destination!.longitude,
         ),
         travelMode: directions_pkg.TravelMode.driving,
+        alternatives: true, // Request alternative routes
       );
 
-      // Print the entire response for debugging
-      print('Directions API response: ${response.toJson()}');
-
       if (response.status == 'OK' && response.routes.isNotEmpty) {
-        final route = response.routes.first;
-
-        if (route.overviewPolyline?.points != null) {
-          final points = route.overviewPolyline!.points;
-          final polylinePoints = PolylinePoints().decodePolyline(points);
-
-          // Create a new set with the updated polyline
-          Set<Polyline> newPolylines = {};
-          newPolylines.add(
-            Polyline(
-              polylineId: PolylineId('route'),
-              points: polylinePoints.map((point) => LatLng(point.latitude, point.longitude)).toList(),
-              width: 5,
-              color: Colors.blue,
-            ),
-          );
-
-          setState(() {
-            _polylines = newPolylines; // Update _polylines with the new set
-          });
-        } else {
-          print('No polyline points found in the route.');
-          _showErrorDialog('No polyline points found in the route.');
-        }
+        setState(() {
+          _routes = response.routes;
+          _polylines.clear(); // Clear existing polylines
+          _showRouteSelectionPanel();
+        });
       } else {
         print('Failed to fetch directions: ${response.errorMessage}');
         _showErrorDialog('Failed to fetch directions. Please try again.');
@@ -171,14 +237,76 @@ class _GoogleMapPageState extends State<GoogleMapPage> {
     }
   }
 
+  void _showRouteSelectionPanel() {
+    showModalBottomSheet(
+      context: context,
+      builder: (BuildContext context) {
+        return ListView.builder(
+          itemCount: _routes.length,
+          itemBuilder: (context, index) {
+            final route = _routes[index];
+            final leg = route.legs.first;
+            final distance = leg.distance?.text ?? '';
+            final duration = leg.duration?.text ?? '';
 
-  Future<List<PlacesSearchResult>> _getPlaces(String query) async {
+            return ListTile(
+              title: Text('Route ${index + 1}'),
+              subtitle: Text('Distance: $distance, Duration: $duration'),
+              onTap: () {
+                Navigator.pop(context); // Close the route selection panel
+                _displayRoute(route);
+              },
+            );
+          },
+        );
+      },
+    );
+  }
+
+  void _displayRoute(directions_pkg.Route route) {
+    setState(() {
+      _polylines.clear(); // Clear existing polylines
+
+      if (route.overviewPolyline?.points != null) {
+        final points = route.overviewPolyline!.points;
+        final polylinePoints = PolylinePoints().decodePolyline(points);
+
+        _polylines.add(Polyline(
+          polylineId: PolylineId('${route.hashCode}'), // Unique ID for each route
+          points: polylinePoints.map((point) => LatLng(point.latitude, point.longitude)).toList(),
+          width: 5,
+          color: Colors.blue,
+        ));
+
+        final leg = route.legs.first;
+        _routeDuration = leg.duration?.text ?? '';
+        _routeDistance = leg.distance?.text ?? '';
+        _showNavigationButton = true; // Show navigation button
+        _showNavigationBottomSheet(_routeDuration, _routeDistance);
+      } else {
+        _showErrorDialog('No valid route found.');
+      }
+    });
+  }
+
+  Future<PlaceDetails> _getPlaceDetails(String placeId) async {
     final places = GoogleMapsPlaces(apiKey: apiKey);
-    final response = await places.searchByText(query);
+    final response = await places.getDetailsByPlaceId(placeId);
 
     if (response.status == 'OK') {
-      print('Places search response: ${response.results}');
-      return response.results;
+      return response.result!;
+    } else {
+      throw Exception('Failed to fetch place details');
+    }
+  }
+
+  Future<List<Prediction>> _getPlaces(String query) async {
+    final places = GoogleMapsPlaces(apiKey: apiKey);
+    final response = await places.autocomplete(query, types: ['geocode']);
+
+    if (response.status == 'OK') {
+      print('Places autocomplete response: ${response.predictions}');
+      return response.predictions;
     } else {
       print('Failed to fetch places: ${response.errorMessage}');
       throw Exception('Failed to fetch places');
@@ -201,9 +329,57 @@ class _GoogleMapPageState extends State<GoogleMapPage> {
     );
   }
 
-  @override
-  void dispose() {
-    _searchController.dispose();
-    super.dispose();
+  void _showNavigationBottomSheet(String durationText, String distanceText) {
+    showModalBottomSheet(
+      context: context,
+      builder: (BuildContext context) {
+        return Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            ListTile(
+              title: Text('Start Navigation'),
+              onTap: () {
+                Navigator.pop(context); // Close the navigation modal sheet
+                _startNavigation();
+              },
+            ),
+            ListTile(
+              title: Text('Cancel'),
+              onTap: () {
+                Navigator.pop(context); // Close the navigation modal sheet
+                setState(() {
+                  _destination = null;
+                  _showNavigationButton = false;
+                  _polylines.clear();
+                });
+              },
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                children: [
+                  Text('Route duration: $durationText'),
+                  Text('Route distance: $distanceText'),
+                ],
+              ),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void _startNavigation() async {
+    if (_destination == null) {
+      _showErrorDialog('Destination is not set.');
+      return;
+    }
+
+    final googleUrl = 'https://www.google.com/maps/dir/?api=1&destination=${_destination!.latitude},${_destination!.longitude}';
+    if (await canLaunch(googleUrl)) {
+      await launch(googleUrl);
+    } else {
+      _showErrorDialog('Could not launch Google Maps.');
+    }
   }
 }
